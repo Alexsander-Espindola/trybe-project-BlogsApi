442c0825d447187c75d7809bc03963a6c6500bf0
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-02-13 20:57:38 -0300

    Gerando o banco de dados blogs_api e criando a tabela users por meio do sequelize

---
+++ models/users.js
@@ -0,0 +1,8 @@
+const Users = (sequelize, DataTypes) => sequelize.define('Users', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+module.exports = Users;
